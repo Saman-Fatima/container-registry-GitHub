1bce2a0b6818eb665ba5f354745c7b2a4d48f6d1
Saman-Fatima <73308738+Saman-Fatima@users.noreply.github.com>
2022-09-22 02:58:12 +0500

    added .ssh in nonroot and changing its owner

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,5 +76,6 @@ fi
 echo mypasswd | sudo -S chown nonroot /home/nonroot/
 echo mypasswd | sudo -S chown -R nonroot /opt/
 mkdir .ssh/
-echo mypasswd |  sudo cp -r /home/nonroot/ssh-mounted/ .ssh/
+echo mypasswd |  sudo cp -r /home/nonroot/ssh-mounted/ /home/nonroot/.ssh/
+echo mypasswd | sudo -S chown -R nonroot /home/nonroot/.ssh/
 exec "$@"
