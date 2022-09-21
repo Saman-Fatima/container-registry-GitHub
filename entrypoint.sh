84c821beb0bfc90a01ea6517ec99bba0b58e1b7c
Saman-Fatima <73308738+Saman-Fatima@users.noreply.github.com>
2022-09-21 18:02:33 +0500

    mount ssh in separate dir and copy files

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,7 +74,7 @@ else
         --unattended
 fi
 echo mypasswd | sudo -S chown nonroot /home/nonroot/
-echo mypasswd | sudo -S chown -R nonroot /home/nonroot/.ssh
 echo mypasswd | sudo -S chown -R nonroot /opt/
-
+mkdir .ssh/
+echo mypasswd |  sudo cp -r ssh-mounted/ .ssh/
 exec "$@"
